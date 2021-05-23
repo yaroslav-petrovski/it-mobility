af04e22e41b02e6fee20f57c214ee19f325ccbb8
Yaroslav <yaroslav25@list.ru>
2021-05-23 11:39:55 +0200

    Add empty activities for QR and select city

---
+++ app/src/main/java/mumayank/com/itmobilityproject/SelectCityActivity.kt
@@ -0,0 +1,11 @@
+package mumayank.com.itmobilityproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SelectCityActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_select_city)
+    }
+}
