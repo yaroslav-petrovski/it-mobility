02b3b78747b8c550f61635d625349ac3bcab9e1f
Yaroslav <yaroslav25@list.ru>
2021-05-27 16:23:44 +0200

    Empty files for views created

---
+++ app/src/main/java/mumayank/com/itmobilityproject/ResultActivity.kt
@@ -0,0 +1,11 @@
+package mumayank.com.itmobilityproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ResultActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_result)
+    }
+}
