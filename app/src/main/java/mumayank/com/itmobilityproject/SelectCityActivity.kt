e776e84b8a87a8c49eea22df323a16af815eec7e
Yaroslav <yaroslav25@list.ru>
2021-06-09 18:23:20 +0200

    Capture city name from edit text

--- app/src/main/java/mumayank/com/itmobilityproject/SelectCityActivity.kt
+++ app/src/main/java/mumayank/com/itmobilityproject/SelectCityActivity.kt
@@ -2,10 +2,17 @@ package mumayank.com.itmobilityproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_select_city.*
 
 class SelectCityActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_select_city)
+
+        getCityName.setOnClickListener {
+            var cityName = CityEditText.text
+            Toast.makeText(this, cityName, Toast.LENGTH_SHORT).show()
+        }
     }
 }
