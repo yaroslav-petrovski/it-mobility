265765ef5978ff6eb11219bcd0f4c83cfd4d002e
Yaroslav <yaroslav25@list.ru>
2021-06-10 00:44:12 +0200

    Select City start intent

--- app/src/main/java/mumayank/com/itmobilityproject/SelectCityActivity.kt
+++ app/src/main/java/mumayank/com/itmobilityproject/SelectCityActivity.kt
@@ -1,5 +1,6 @@
 package mumayank.com.itmobilityproject
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
@@ -11,8 +12,21 @@ class SelectCityActivity : AppCompatActivity() {
         setContentView(R.layout.activity_select_city)
 
         getCityName.setOnClickListener {
-            var cityName = CityEditText.text
+            var cityName = CityEditText.text.toString()
             Toast.makeText(this, cityName, Toast.LENGTH_SHORT).show()
+            //println("TESTTEST____$cityName")
+            nextActivity(cityName)
+
         }
+    }
+
+    private fun nextActivity(cityName: String){
+        val intent = Intent(this, StartActivity::class.java)
+        intent.putExtra("City", cityName)
+        /*intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK)
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)*/
+        startActivity(intent)
+        overridePendingTransition(0, 0)
     }
 }
