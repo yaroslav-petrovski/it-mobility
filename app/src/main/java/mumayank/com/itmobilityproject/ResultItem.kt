62b40d45721fcebd5bec41476f6e3ac2bc0d37f6
Yaroslav <yaroslav25@list.ru>
2021-06-15 21:42:24 +0200

    Comment ResultItem

--- app/src/main/java/mumayank/com/itmobilityproject/ResultItem.kt
+++ app/src/main/java/mumayank/com/itmobilityproject/ResultItem.kt
@@ -1,5 +1,16 @@
 package mumayank.com.itmobilityproject
 
+/**
+ * Result item
+ *
+ * Class to store the shop information from the database
+ *
+ * @property Adress adress of the shop
+ * @property cntProducts number of searched product
+ * @property lat latitude (shop position)
+ * @property long longitude (shop position)
+ * @property distanceToUser distance between user and shop (calculated in Result activity)
+ */
 class ResultItem(
     val Adress: String,
     val cntProducts: Int,
