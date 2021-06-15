90ef907cf11ef97bd681ea1e300925e1fc0a748e
Yaroslav <yaroslav25@list.ru>
2021-06-15 22:00:02 +0200

    Comment ResultListAdapter

--- app/src/main/java/mumayank/com/itmobilityproject/ResultListAdapter.kt
+++ app/src/main/java/mumayank/com/itmobilityproject/ResultListAdapter.kt
@@ -6,6 +6,11 @@ import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.TextView
 
+/**
+ * Result list adapter
+ *
+ * Adapt List with shops to the listView
+ */
 class ResultListAdapter(private val context: Activity, private val items: MutableList<ResultItem>) :
     ArrayAdapter<ResultItem>(context, R.layout.list_row, items) {
 
@@ -13,15 +18,28 @@ class ResultListAdapter(private val context: Activity, private val items: Mutabl
         val inflater = context.layoutInflater
         val rowView = inflater.inflate(R.layout.list_row, null, true)
 
+        /**
+         * TextViews from layout
+         */
         val adressText = rowView.findViewById(R.id.adress) as TextView
         val cntText = rowView.findViewById(R.id.cnt) as TextView
         val distText = rowView.findViewById(R.id.distance) as TextView
 
+        /**
+         * set address textView
+         */
         adressText.text = items[position].Adress
 
+        /**
+         * set product count textView
+         */
         val stringForCntText = items[position].cntProducts.toString() + " products left"
         cntText.text = stringForCntText
 
+        /**
+         * Set distance textView
+         * If user position unknown: hide textView
+         */
         if (items[position].distanceToUser == 0.0F)
             distText.visibility = View.INVISIBLE
         val stringForDistText = String.format("%.2f", items[position].distanceToUser / 1000) + " km"
